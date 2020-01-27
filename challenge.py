1d833e5510063fdb4719a1d660b8b469ade3a50e
Emilio Araos <ea3dev@gmail.com>
2020-01-27 03:47:48 -0700

    reading and writing

--- challenge.py
+++ challenge.py
@@ -1,4 +1,4 @@
-with open("sample.txt", 'a') as table_two:
+with open("sample2.txt", 'w') as table_two:
     for i in range(2,13):
         for j in range(1,13):
             print("{1:>2} times  {0}  is {2}".format(i, j, i * j), file=table_two)
